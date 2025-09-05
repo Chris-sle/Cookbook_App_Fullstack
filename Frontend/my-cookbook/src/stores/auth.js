0d59a686465c6633e64fd7fff1f6163df73197a7
Chris <oscar.sletvold@gmail.com>
2025-09-05 15:03:40 +0200

    Update auth.js
    fleshed out a bit more, and added logout

--- Frontend/my-cookbook/src/stores/auth.js
+++ Frontend/my-cookbook/src/stores/auth.js
@@ -3,21 +3,38 @@ import { defineStore } from 'pinia'
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     token: localStorage.getItem('token') || '',
-    userId: null,
+    userId: localStorage.getItem('userId') || null,
+    username: localStorage.getItem('username') || null,
   }),
 
   actions: {
-    setToken(token) {
+    setToken(token, { userId, username } = {}) {
       this.token = token
       localStorage.setItem('token', token)
+
+      if (userId !== undefined) {
+        this.userId = userId
+        localStorage.setItem('userId', userId)
+      }
+      if (username !== undefined) {
+        this.username = username
+        localStorage.setItem('username', username)
+      }
     },
     clearToken() {
       this.token = ''
+      this.userId = null
+      this.username = null
       localStorage.removeItem('token')
+      localStorage.removeItem('userId')
+      localStorage.removeItem('username')
     },
-    setUserId(id) {
-      this.userId = id
-    }
+    setUser({ userId, username }) {
+      this.userId = userId
+      this.username = username
+      localStorage.setItem('userId', userId)
+      localStorage.setItem('username', username)
+    },
   },
 
   getters: {
