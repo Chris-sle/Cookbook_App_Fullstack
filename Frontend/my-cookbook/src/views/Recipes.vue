74bc4b640b558f72b9d2328cdf1276c965cd4264
Chris <oscar.sletvold@gmail.com>
2025-10-16 15:24:20 +0200

    Update Recipes.vue

--- Frontend/my-cookbook/src/views/Recipes.vue
+++ Frontend/my-cookbook/src/views/Recipes.vue
@@ -1,1 +1,22 @@
-<template></template>
+<template>
+  <div class="recipes-view">
+    <h1>Recipes</h1>
+    <RecipeList />
+  </div>
+</template>
+
+<script setup>
+import RecipeList from '../components/Recipes/RecipeList.vue'
+</script>
+
+<style scoped>
+.recipes-view {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px 12px;
+}
+.recipes-view h1 {
+  margin: 0 0 16px;
+  font-size: 1.5rem;
+}
+</style>
