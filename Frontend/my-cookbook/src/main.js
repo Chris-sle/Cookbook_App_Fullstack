f43d873e904a19b5d79d70450d3e2c4fd4110786
Chris <oscar.sletvold@gmail.com>
2025-10-16 23:44:49 +0200

    Update main.js

--- Frontend/my-cookbook/src/main.js
+++ Frontend/my-cookbook/src/main.js
@@ -1,13 +1,25 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
 import { createPinia } from 'pinia'
+import App from './App.vue'
+import api from './services/api'
 import router from './router'
+import './style.css'
 
 const app = createApp(App)
 
 const pinia = createPinia()
 app.use(pinia)
 app.use(router)
+
+// Attach interceptor after store is set up
+const { useAuthStore } = await import('./stores/auth')
+const auth = useAuthStore()
+
+api.interceptors.request.use((config) => {
+  if (auth.token) {
+    config.headers['x-auth-token'] = auth.token
+  }
+  return config
+})
 
 app.mount('#app')
