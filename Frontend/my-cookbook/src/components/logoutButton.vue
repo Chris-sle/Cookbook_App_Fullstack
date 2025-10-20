2ba3a5d68e404c9fcaab555d3deeab08137e130d
Chris <oscar.sletvold@gmail.com>
2025-10-20 14:50:40 +0200

    Create logoutButton.vue

---
+++ Frontend/my-cookbook/src/components/logoutButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <button @click="logout">Logout</button>
+</template>
+
+<script setup>
+import { useAuthStore } from '../stores/auth'
+import api from '../services/api'
+
+const auth = useAuthStore()
+
+async function logout() {
+    try {
+        await api.post('/auth/logout')
+    } catch (err) {
+        console.warn('Logout API call failed:', err)
+    }
+    auth.clearToken()
+}
+</script>
